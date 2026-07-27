4069ffd9053daecfbcb5f2c44c59f2f6ba579bec
navinnova54-sudo <navinnova54@gmail.com>
2026-07-27 10:08:47 +0530

    Update ModifierDemo.java

---
+++ Day 11/ModifierDemo.java
@@ -0,0 +1,23 @@
+package Day11;
+
+class AccessExample {
+
+    public int a = 10;
+    protected int b = 20;
+    int c = 30;
+    private int d = 40;
+
+    void display() {
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+    }
+}
+
+public class ModifierDemo1 {
+    public static void main(String[] args) {
+        AccessExample obj = new AccessExample();
+        obj.display();
+    }
+}
