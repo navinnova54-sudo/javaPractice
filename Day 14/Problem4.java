d05702729565447b0ad7d5057af042334ca20832
navinnova54-sudo <navinnova54@gmail.com>
2026-07-28 21:17:05 +0530

    Implement division with zero handling

---
+++ Day 14/Problem4.java
@@ -0,0 +1,10 @@
+public class Problem4 {
+    public static void main(String[] args) {
+        try {
+            int a = 10, b = 0;
+            System.out.println(a / b);
+        } catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+    }
+}
