bfbe9bab525bd7fef5e9e9ba19a363bfb9534d60
navinnova54-sudo <navinnova54@gmail.com>
2026-07-28 21:20:18 +0530

    Update Problem1.java

---
+++ Day 16/Problem1.java
@@ -0,0 +1,12 @@
+public class Problem1 {
+    public int passThePillow(int n, int time) {
+        int cycle = n - 1;
+        int pos = time % (2 * cycle);
+
+        if (pos <= cycle) {
+            return pos + 1;
+        } else {
+            return n - (pos - cycle);
+        }
+    }
+}
